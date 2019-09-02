7edb85d5a169e3f65bed21fd38fa5962cdd22e31
lesroad <342705814@qq.com>
2019-09-02 11:14:37 +0800

    master

---
+++ master/main/master.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"crontab/master"
+	"flag"
+	"fmt"
+	"runtime"
+	"time"
+)
+
+var (
+	confFile string // 配置文件的路径
+)
+
+// 解析命令行参数
+func initArgs() {
+	// master -config ./master.json
+	// params 存到哪，指定参数，参数默认值，-h输出提示
+	// master -h
+	flag.StringVar(&confFile, "config", "./master.json", "指定master.json")
+	flag.Parse()
+}
+
+// 初始化线程数量
+func initEvn() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+}
+
+func main() {
+	var (
+		err error
+	)
+	// 初始化命令行参数
+	initArgs()
+
+	// 初始化线程
+	initEvn()
+
+	// 加载配置
+	if err = master.InitConfig(confFile); err != nil {
+		goto ERR
+	}
+
+	// 初始化服务发现模块
+	if err = master.InitWorkerMgr(); err != nil {
+		goto ERR
+	}
+
+	// 日志管理器
+	if err = master.InitLogMgr(); err != nil {
+		goto ERR
+	}
+
+	// 任务管理器
+	if err = master.InitJobMgr(); err != nil {
+		goto ERR
+	}
+
+	// 启动Api HTTP服务
+	if err = master.InitApiServer(); err != nil {
+		goto ERR
+	}
+
+	// 正常退出
+	for {
+		time.Sleep(1 * time.Second)
+	}
+
+ERR:
+	fmt.Println(err)
+}
